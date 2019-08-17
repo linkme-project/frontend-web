e60c0eb328666a6e4dde21da5e1dbf7cc59eb446
Keunhak Lim <limkeunhak@gmail.com>
2019-08-17 16:31:53 +0900

    Initial commit

---
+++ backend/src/main/java/com/linkme/demo/LinkmeApplication.java
@@ -0,0 +1,13 @@
+package com.linkme.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkmeApplication.class, args);
+	}
+
+}
